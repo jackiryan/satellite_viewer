5d3d4d9998e475c366a783140c7eb95784b6e49f
jackiryan <Jacqueline.Ryan@jpl.nasa.gov>
2024-07-15 14:44:38 -0700

    Fixed the axis convention, fixed day/night cycle, added another satellite (turn this into a function)

--- viewer.js
+++ viewer.js
@@ -6,7 +6,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 // Initialize scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -27,9 +29,7 @@ function dayOfYear(date) {
     return day;
 }
 
-function getSolarDeclinationAngle() {
-    // Get the current date
-    const date = new Date();
+function getSolarDeclinationAngle(date) {
     const N = dayOfYear(date);
     const obliquityAngle = 23.44 * Math.PI / 180.0;
     // Simplified formula for calculating declination angle https://solarsena.com/solar-declination-angle-calculator/
@@ -40,16 +40,26 @@ function getSolarDeclinationAngle() {
 function getTwilightAngle() {
     // Civil, Nautical, and Astronomical Twilight account for sun angles up to about 18 degrees past the horizon
     // For some reason doubling the number gives me a result that's closer to reality
-    return 18.0 * Math.PI / 180.0;
+    return 36.0 * Math.PI / 180.0;
 }
+
+//const now = new Date(Date.UTC(2024,2,24,3,6,0,0)); // Vernal Equinox 2024, helpful for testing
+const now = new Date(); // Get current time
+console.log(getSolarDeclinationAngle(now));
+// Use satelliteJS to get the sidereal time, which describes a rotation
+const gmst = satellite.gstime(now);
+const hours = now.getUTCHours() + now.getUTCMinutes() / 60 + now.getUTCSeconds() / 3600;
+const solarTime = ((hours - 12) / 24) * 2 * Math.PI;
 
 // Shader material
 const material = new THREE.ShaderMaterial({
     uniforms: {
         dayTexture: { type: 't', value: dayTexture },
         nightTexture: { type: 't', value: nightTexture },
-        declinationAngle: { type: 'f', value: getSolarDeclinationAngle() },
-        twilightAngle: { type: 'f', value: getTwilightAngle() }
+        declinationAngle: { type: 'f', value: getSolarDeclinationAngle(now) },
+        twilightAngle: { type: 'f', value: getTwilightAngle() },
+        gmst: { type: 'f', value: gmst },
+        solarTime: { type: 'f', value: solarTime }
     },
     vertexShader: `
         varying vec2 vUv;
@@ -61,50 +71,30 @@ const material = new THREE.ShaderMaterial({
             gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         }
     `,
-    /*
-    fragmentShader: `
-        uniform sampler2D dayTexture;
-        uniform sampler2D nightTexture;
-        uniform float declinationAngle;
-        uniform float twilightAngle;
-        varying vec2 vUv;
-        varying vec3 vPosition;
-
-        void main() {
-            float cosAngle = cos(-declinationAngle);
-            float sinAngle = sin(-declinationAngle);
-            float rotatedX = vPosition.x * cosAngle - vPosition.y * sinAngle;
-            vec3 dayColor = texture2D(dayTexture, vUv).rgb;
-            vec3 nightColor = texture2D(nightTexture, vUv).rgb;
-
-            // Blend between day and night offset over the 18 degrees of twilight, biased towards the night side
-            // Add a 4 degree nudge since civil twilight seems to start when the sun is 2 degrees below the horizon
-            // Use 4 because doubling the numbers made it look better visually compared to online resources
-            float blendFactor = clamp((3.1415926 / 90.0) + (rotatedX + twilightAngle) / twilightAngle, 0.0, 1.0);
-            vec3 color = mix(nightColor, dayColor, blendFactor);
-            gl_FragColor = vec4(color, 1.0);
-        }
-    `,
-    */
     fragmentShader: `
     uniform sampler2D dayTexture;
     uniform sampler2D nightTexture;
     uniform float declinationAngle;
     uniform float twilightAngle;
+    uniform float gmst;
+    uniform float solarTime;
     varying vec2 vUv;
     varying vec3 vPosition;
 
     void main() {
         float cosAngle = cos(-declinationAngle);
         float sinAngle = sin(-declinationAngle);
-        float rotatedX = vPosition.y * cosAngle - vPosition.z * sinAngle;
+        // The sphere is rotated by the gmst in scene space, which cares about sidereal time, but we
+        // want a "solar time", so back out the gmst rotation and add in a rotation representing the 
+        // difference between solar noon at the prime meridian and the time now.
+        float rotatedX = vPosition.x * cos(-gmst + solarTime) + vPosition.z * sin(-gmst + solarTime);
+        float rotPos = rotatedX * cosAngle - vPosition.y * sinAngle;
         vec3 dayColor = texture2D(dayTexture, vUv).rgb;
         vec3 nightColor = texture2D(nightTexture, vUv).rgb;
 
         // Blend between day and night offset over the 18 degrees of twilight, biased towards the night side
-        // Add a 4 degree nudge since civil twilight seems to start when the sun is 2 degrees below the horizon
-        // Use 4 because doubling the numbers made it look better visually compared to online resources
-        float blendFactor = clamp((3.1415926 / 90.0) + (rotatedX + twilightAngle) / twilightAngle, 0.0, 1.0);
+        // Note that the degrees of twilight is doubled for aesthetic reasons
+        float blendFactor = clamp((rotPos + twilightAngle) / twilightAngle, 0.0, 1.0);
         vec3 color = mix(nightColor, dayColor, blendFactor);
         gl_FragColor = vec4(color, 1.0);
     }
@@ -114,12 +104,13 @@ const material = new THREE.ShaderMaterial({
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
-const now = new Date(); // Current time
 // Satellite JS Example TLE Data
-const tleLine1 = "1 25544U 98067A   24196.87887973  .00024315  00000+0  43401-3 0  9998";
-const tleLine2 = "2 25544  51.6406 178.1499 0010661  68.1448  15.0403 15.49862093462842";
+const tleLine1 = "1 25544U 98067A   24197.73434340 -.00003641  00000+0 -55873-4 0  9998";
+const tleLine2 = "2 25544  51.6386 173.9126 0010242  70.8365 108.5623 15.49867467462978";
 const tleLine3 = "1 43873U 18109A   24196.25380453 -.00000006  00000+0  00000+0 0  9991";
 const tleLine4 = "2 43873  55.3065 119.9592 0029353 194.3360 337.8959  2.00557396 40976";
+const tleLine5 = "1 43252U 18030D   24197.61300312  .00000155  00000+0  48304-4 0  9990";
+const tleLine6 = "2 43252  86.3962 223.6120 0002684  90.2873 269.8630 14.34218458329634";
 
 // Calculate Satellite Position
 const satrec = satellite.twoline2satrec(tleLine1, tleLine2);
@@ -128,8 +119,8 @@ const positionEci = positionAndVelocity.position;
 const scaleFactor = radius / 6371;
 const scenePosition = new THREE.Vector3(
     positionEci.x * scaleFactor,
-    positionEci.y * scaleFactor,
-    positionEci.z * scaleFactor
+    positionEci.z * scaleFactor,
+    -positionEci.y * scaleFactor
 );
 
 // Create ECI Satellite Mesh
@@ -145,8 +136,8 @@ const positionAndVelocity2 = satellite.propagate(satrec2, now);
 const positionEci2 = positionAndVelocity2.position;
 const scenePosition2 = new THREE.Vector3(
     positionEci2.x * scaleFactor,
-    positionEci2.y * scaleFactor,
-    positionEci2.z * scaleFactor
+    positionEci2.z * scaleFactor,
+    -positionEci2.y * scaleFactor
 );
 
 // Create ECI Satellite Mesh
@@ -155,6 +146,23 @@ const satMaterial2 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const twosat = new THREE.Mesh(satGeometry2, satMaterial2);
 twosat.position.copy(scenePosition2);
 scene.add(twosat);
+
+// Calculate Satellite Position
+const satrec3 = satellite.twoline2satrec(tleLine5, tleLine6);
+const positionAndVelocity3 = satellite.propagate(satrec3, now);
+const positionEci3 = positionAndVelocity3.position;
+const scenePosition3 = new THREE.Vector3(
+    positionEci3.x * scaleFactor,
+    positionEci3.z * scaleFactor,
+    -positionEci3.y * scaleFactor
+);
+
+// Create ECI Satellite Mesh
+const satGeometry3 = new THREE.SphereGeometry(0.1, 16, 16);
+const satMaterial3 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const threesat = new THREE.Mesh(satGeometry3, satMaterial3);
+threesat.position.copy(scenePosition3);
+scene.add(threesat);
 
 // Position the camera
 camera.position.z = 10;
@@ -166,11 +174,7 @@ scene.add(ambientLight);
 // Add controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// Determine the initial rotation of the sphere based on the current time on Earth
-// Calculate time differences
-const gmst = satellite.gstime(now);
-sphere.rotation.x = Math.PI / 2;
-//sphere.rotation.z = -Math.PI / 2;
+// Determine the initial rotation of the sphere based on the current sidereal time
 sphere.rotation.y = gmst;
 /*
 const msPerDay = 86400000.0; // Number of milliseconds in a day
