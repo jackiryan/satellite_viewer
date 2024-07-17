f34fbf78f937b877a3b606b9feac7354fe158fcb
Jacqueline Ryan <Jacqueline.Ryan@jpl.nasa.gov>
2024-07-16 23:21:03 -0700

    Deleted accidental satplot file; viewer was refactored

--- viewer.js
+++ viewer.js
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import * as satellite from 'satellite.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { cameraPosition } from 'three/examples/jsm/nodes/Nodes.js';
+import { add } from 'three/examples/jsm/libs/tween.module.js';
 
 // Initialize scene, camera, and renderer
 const scene = new THREE.Scene();
@@ -78,6 +79,14 @@ function getSunPointingAngle(siderealTime, solarTime, declinationAngle) {
 }
 
 //const now = new Date(Date.UTC(2024,2,24,3,6,0,0)); // Vernal Equinox 2024, helpful for testing
+/*
+// Create a test plane for checking sidereal vs solar day issues.
+const planeGeometry = new THREE.BoxGeometry(0.1, 10, 10);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+*/
+
 const now = new Date(); // Get current time
 // Use satelliteJS to get the sidereal time, which describes a rotation
 const gmst = satellite.gstime(now);
@@ -154,79 +163,67 @@ scene.add(sunHelper);
 /* Satellite code -- very rough */
 
 // Satellite JS Example TLE Data
-const tleLine1 = "1 25544U 98067A   24197.73434340 -.00003641  00000+0 -55873-4 0  9998";
-const tleLine2 = "2 25544  51.6386 173.9126 0010242  70.8365 108.5623 15.49867467462978";
-const tleLine3 = "1 43873U 18109A   24196.25380453 -.00000006  00000+0  00000+0 0  9991";
-const tleLine4 = "2 43873  55.3065 119.9592 0029353 194.3360 337.8959  2.00557396 40976";
-const tleLine5 = "1 43252U 18030D   24197.61300312  .00000155  00000+0  48304-4 0  9990";
-const tleLine6 = "2 43252  86.3962 223.6120 0002684  90.2873 269.8630 14.34218458329634";
+const tleLines = [
+    // ISS (ZARYA)
+    "1 25544U 98067A   24197.73434340 -.00003641  00000+0 -55873-4 0  9998",
+    "2 25544  51.6386 173.9126 0010242  70.8365 108.5623 15.49867467462978",
+    // Some high altitude satellite (I forgor)
+    "1 43873U 18109A   24196.25380453 -.00000006  00000+0  00000+0 0  9991",
+    "2 43873  55.3065 119.9592 0029353 194.3360 337.8959  2.00557396 40976",
+    // IRIDIUM-140
+    "1 43252U 18030D   24197.61300312  .00000155  00000+0  48304-4 0  9990",
+    "2 43252  86.3962 223.6120 0002684  90.2873 269.8630 14.34218458329634"
+];
 
-// Calculate Satellite Position
-const satrec = satellite.twoline2satrec(tleLine1, tleLine2);
-const positionAndVelocity = satellite.propagate(satrec, now);
-const positionEci = positionAndVelocity.position;
-const velocityEci = positionAndVelocity.velocity;
 const scaleFactor = radius / 6371;
-const scenePosition = new THREE.Vector3(
-    positionEci.x * scaleFactor,
-    positionEci.z * scaleFactor,
-    -positionEci.y * scaleFactor
-);
-const instVelocity = new THREE.Vector3(
-    velocityEci.x * scaleFactor,
-    velocityEci.z * scaleFactor,
-    -velocityEci.y * scaleFactor
-);
+function addSatellite(satrec, color) {
+    const positionAndVelocity = satellite.propagate(satrec, now);
+    // This app uses ECI coordinates, so there is no need to convert to Geodetic
+    const positionEci = positionAndVelocity.position;
+    // Create Satellite Mesh and copy in an initial position
+    const satGeometry = new THREE.SphereGeometry(0.1, 16, 16);
+    const satMaterial = new THREE.MeshBasicMaterial(color);
+    const scenePosition = new THREE.Vector3(
+        positionEci.x * scaleFactor,
+        positionEci.z * scaleFactor,
+        -positionEci.y * scaleFactor
+    );
+    const sat = new THREE.Mesh(satGeometry, satMaterial);
+    sat.position.copy(scenePosition);
+    return sat;
+}
 
-// Create ECI Satellite Mesh
-const satGeometry = new THREE.SphereGeometry(0.1, 16, 16);
-const satMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const onesat = new THREE.Mesh(satGeometry, satMaterial);
-onesat.position.copy(scenePosition);
-scene.add(onesat);
+// Modifies the position of a given satellite mesh sat with the propagated SPG4
+// position at time t, as a side effect. No retval.
+function updateSatellitePosition(satrec, sat, t) {
+    const deltaPosVel = satellite.propagate(satrec, t);
+    const deltaPosEci = deltaPosVel.position;
+    const deltaPos = new THREE.Vector3(
+        deltaPosEci.x * scaleFactor,
+        deltaPosEci.z * scaleFactor,
+        -deltaPosEci.y * scaleFactor
+    );
+    sat.position.copy(deltaPos);
+}
 
-// Calculate Satellite Position
-const satrec2 = satellite.twoline2satrec(tleLine3, tleLine4);
-const positionAndVelocity2 = satellite.propagate(satrec2, now);
-const positionEci2 = positionAndVelocity2.position;
-const scenePosition2 = new THREE.Vector3(
-    positionEci2.x * scaleFactor,
-    positionEci2.z * scaleFactor,
-    -positionEci2.y * scaleFactor
-);
-
-// Create ECI Satellite Mesh
-const satGeometry2 = new THREE.SphereGeometry(0.1, 16, 16);
-const satMaterial2 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const twosat = new THREE.Mesh(satGeometry2, satMaterial2);
-twosat.position.copy(scenePosition2);
-scene.add(twosat);
-
-// Calculate Satellite Position
-const satrec3 = satellite.twoline2satrec(tleLine5, tleLine6);
-const positionAndVelocity3 = satellite.propagate(satrec3, now);
-const positionEci3 = positionAndVelocity3.position;
-const scenePosition3 = new THREE.Vector3(
-    positionEci3.x * scaleFactor,
-    positionEci3.z * scaleFactor,
-    -positionEci3.y * scaleFactor
-);
-
-// Create ECI Satellite Mesh
-const satGeometry3 = new THREE.SphereGeometry(0.1, 16, 16);
-const satMaterial3 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const threesat = new THREE.Mesh(satGeometry3, satMaterial3);
-threesat.position.copy(scenePosition3);
-scene.add(threesat);
-
-/*
-// Create a test plane for checking sidereal vs solar day issues.
-const planeGeometry = new THREE.BoxGeometry(0.1, 10, 10);
-const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-scene.add(plane);
-*/
-
+const satrecs = [];
+const satellites = [];
+const colors = [
+    { color: 0xff0000 },
+    { color: 0x00ff00 },
+    { color: 0x0000ff }
+];
+// Create satellites one at a time, eventually this should be BufferGeometry
+for (let i = 0; i < tleLines.length; i += 2) {
+    let satreci = satellite.twoline2satrec(
+        tleLines[i],
+        tleLines[i+1]
+    );
+    satrecs.push(satreci);
+    let sat = addSatellite(satreci, colors[i/2]);
+    satellites.push(sat);
+    scene.add(satellites.at(-1));
+}
 
 // Add ambient light
 const ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
@@ -234,6 +231,8 @@ scene.add(ambientLight);
 
 // Add controls
 const controls = new OrbitControls(camera, renderer.domElement);
+
+// Controls should be disabled when tracking a satellite
 controls.enablePan = false;
 //controls.enableZoom = false;
 
@@ -245,18 +244,23 @@ const rotationRate = (2 * Math.PI) / siderealDaySeconds;
 
 // Factor to run the rotation faster than real time, 3600 ~= 1 rotation/minute
 const speedFactor = 60;
-const satelliteFrameRate = 30.0; // frames per second
+const renderFrameRate = 30.0; // frames per second
 var elapsedSecond = 0;
 var elapsedTime = 0;
+// Camera is fixed inertial reference, globe rotates
+camera.position.z = -10;
+controls.update();
+/* Satellite following code for camera
 const dist = 3;
 const cameraOffset = new THREE.Vector3(
-    threesat.position.x,
-    threesat.position.y,
-    threesat.position.z
-).multiplyScalar(1 + (dist / threesat.position.length()));
+    satellites[0].position.x,
+    satellites[0].position.y,
+    satellites[0].position.z
+).multiplyScalar(1 + (dist / satellites[2].position.length()));
 camera.position.copy(cameraOffset);
-threesat.getWorldPosition(controls.target);
+satellites[0].getWorldPosition(controls.target);
 controls.update();
+*/
 
 // Function to animate the scene
 function animate() {
@@ -269,7 +273,9 @@ function animate() {
     //sphere.rotation.y += rotationRate * scaledDelta;
     elapsedTime += scaledDelta;
     elapsedSecond += scaledDelta;
-    if (elapsedSecond >= speedFactor / satelliteFrameRate) {
+    // This is jank, use a render clock if you want fixed frame rate
+    if (elapsedSecond >= speedFactor / renderFrameRate) {
+        // Update the rotations of things
         const deltaNow = new Date(now.getTime() + elapsedTime * 1000);
         const deltaGmst = satellite.gstime(deltaNow);
         const deltaSolarT = getSolarTime(deltaNow);
@@ -279,61 +285,29 @@ function animate() {
         uniforms.declinationAngle.value = deltaSolarD;
         uniforms.gmst.value = deltaGmst;
         uniforms.solarTime.value = deltaSolarT;
-        const deltaPosVel = satellite.propagate(satrec, deltaNow);
-        const deltaPosVel3 = satellite.propagate(satrec3, deltaNow);
-        const deltaPosEci = deltaPosVel.position;
-        const deltaPosEci3 = deltaPosVel3.position;
-        const deltaPos = new THREE.Vector3(
-            deltaPosEci.x * scaleFactor,
-            deltaPosEci.z * scaleFactor,
-            -deltaPosEci.y * scaleFactor
-        );
-        const deltaPos3 = new THREE.Vector3(
-            deltaPosEci3.x * scaleFactor,
-            deltaPosEci3.z * scaleFactor,
-            -deltaPosEci3.y * scaleFactor
-        )
-        onesat.position.copy(deltaPos);
-        threesat.position.copy(deltaPos3);
+
+        // Update satellite positions
+        for (let j = 0; j < satellites.length; j++) {
+            updateSatellitePosition(
+                satrecs[j],
+                satellites[j],
+                deltaNow
+            );
+        }
+
+        /* Satellite following code for camera
         const cameraOffset = new THREE.Vector3(
-            threesat.position.x,
-            threesat.position.y,
-            threesat.position.z
-        ).multiplyScalar(1 + (dist / threesat.position.length()));
+            satellites[0].position.x,
+            satellites[0].position.y,
+            satellites[0].position.z
+        ).multiplyScalar(1 + (dist / satellites[2].position.length()));
         camera.position.copy(cameraOffset);
-        threesat.getWorldPosition(controls.target);
+        satellites[0].getWorldPosition(controls.target);
         controls.update();
-        //console.log(camera.position);
-        /* Weird camera control experimentation
-        const deltaCam = new THREE.Vector3(
-            -10 * Math.cos(-deltaGmst),
-            camera.position.y,
-            -10 * Math.sin(-deltaGmst)
-        );
-        camera.position.copy(deltaCam);
-        controls.update();
-        // Update the controls target to the object's position
-        //camera.position.copy(threesat.position).add(cameraOffset);
-        //controls.update();
-        //camera.position.copy(threesat.position + cameraToSat);
-        //controls.update();
         */
         elapsedSecond = 0;
     }
     renderer.render(scene, camera);
-}
-
-// Function to position the camera based on the user's time zone offset. No need to use precise location information for a toy.
-function getUserLongitude() {
-    const now = new Date();
-    // function returns value in mins, convert to hours
-    const timeZoneOffset = now.getTimezoneOffset() / 60;
-
-    // Longitude corresponding to the user's time zone
-    // Each hour difference corresponds to 15 degrees of longitude
-    // Convert this value to radians
-    const longitude = (-timeZoneOffset * 15) * Math.PI / 180.0;
-    return longitude;
 }
 
 // Handle window resize
