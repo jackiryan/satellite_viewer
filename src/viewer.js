0acfc8a12a97402759e53dbb4828dbff15910c0b
Jacqueline Ryan <Jacqueline.Ryan@jpl.nasa.gov>
2024-08-11 16:46:50 -0700

    Added fonts to html

--- src/viewer.js
+++ src/viewer.js
@@ -55,7 +55,7 @@ async function init() {
 
     // The camera will be in a fixed intertial reference, so the Earth will rotate
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = -10;
+    camera.position.z = -13;
 
     renderer = new THREE.WebGLRenderer({
         antialias: true
