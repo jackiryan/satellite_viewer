2481e807d01262af110dc7ce16fc9c71d2d67032
Jacqueline Ryan <Jacqueline.Ryan@jpl.nasa.gov>
2024-08-17 10:34:45 -0700

    Working 16-bit version of star shader, demo in nightsky.html

--- src/shaders/skybox/skyFragment.glsl
+++ src/shaders/skybox/skyFragment.glsl
@@ -1,11 +1,127 @@
+// Star Shader
+// (c) 2024 Alex Stone-Martinez & Jacqueline Ryan
+  
+// Permission is hereby granted, free of charge, to any person obtaining a copy of 
+// this software and associated documentation files (the “Software”), to deal in the
+// Software without restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so, subject to the 
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies
+// or substantial portions of the Software.
 
-uniform samplerCube skyboxCubemap;
-uniform float rotY;
-uniform float rotX;
-uniform float rotZ;
-uniform float speed;
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+uniform sampler2D uYoffData;
+uniform sampler2D uXoffData;
+uniform sampler2D uMagData;
+uniform sampler2D uTempData;
+uniform float uPixelSize;
+uniform float uSigma;
+uniform float uScaleFactor;
+uniform float uBrightnessScale;
+
+uniform samplerCube uSkyboxCubemap;
+uniform float uRotY;
+uniform float uRotX;
+uniform float uRotZ;
+uniform float uMwBright;
 
 varying vec3 vDirection;
+
+ float log10(float x) {
+    return log(x) / log(10.0);
+ }
+
+float decodeMagnitude(float encodedValue) {
+    float maxMag = -1.46; // Brightest
+    float minMag = 14.0; // Dimmest
+    if (encodedValue == 0.0) {
+        return 30.0;
+    } else {
+        return (minMag + (minMag - maxMag) * ((encodedValue) * -1.0));
+    }
+}
+
+float magnitudeToBrightness(float magnitude) {
+    // Convert astronomical magnitude to linear brightness
+    // Remember: Lower magnitude means brighter star
+    return pow(10.0, (0.0 - magnitude) / 2.5);
+}
+
+float drawStar(float dist, float sigma) {
+    float gaussianIntensity = exp(-pow(dist, 2.0) / (2.0 * sigma * sigma));
+    return gaussianIntensity;
+}
+
+vec4 getPixInfo(vec3 dir, vec2 offset) {
+    float Px = -1.0 * dir.x;
+    float Py = dir.y;
+    float Pz = dir.z;
+
+    float sumAbs = abs(Px) + abs(Py) + abs(Pz);
+    vec3 Pprime = vec3(Px, Py, Pz) / sumAbs;
+
+    vec2 coord;
+    if (Pprime.z >= 0.0) {
+        coord = Pprime.xy;
+    } else {
+        coord = vec2(
+            sign(Pprime.x) * (1.0 - abs(Pprime.y)),
+            sign(Pprime.y) * (1.0 - abs(Pprime.x))
+        );
+    }
+
+    float u = (coord.x + 1.0) * 0.5;
+    float v = (coord.y + 1.0) * 0.5;
+
+    vec2 pixelSpaceUV = vec2(u, v) * uPixelSize;
+    vec2 pixelCenter = (ceil(pixelSpaceUV) - 0.5) + offset;
+    vec2 uvCenter = pixelCenter / uPixelSize;
+
+    float magData = texture(uMagData, uvCenter).r;
+    vec3 tempData = texture(uTempData, uvCenter).rgb;
+    float xData = texture(uXoffData, uvCenter).r;
+    float yData = texture(uYoffData, uvCenter).r;
+
+    float starBrightness = magnitudeToBrightness(decodeMagnitude(magData) - uScaleFactor);
+
+    vec2 coord1;
+    // Value is between -2.5 / uPixelSize to 2.5 / uPixelSize
+    coord1.x = (uvCenter.x * 2.0 - 1.0) + (((xData * 3.0) - 1.5) / uPixelSize);
+    coord1.y = (uvCenter.y * 2.0 - 1.0) + (((yData * 3.0) - 1.5) / uPixelSize);
+
+    vec3 Pprime1;
+    if (abs(coord1.x) + abs(coord1.y) <= 1.0) {
+        // Original condition for Pprime.z >= 0
+        Pprime1.xy = coord1;
+        Pprime1.z = 1.0 - abs(coord1.x) - abs(coord1.y); // Invert original front-facing projection
+    } else { 
+        // Original condition for Pprime.z < 0, needs guessing
+        // This branch is trickier because the original transformation compresses more information into the same space
+        Pprime1.x = sign(coord1.x) * (1.0 - abs(coord1.y));
+        Pprime1.y = sign(coord1.y) * (1.0 - abs(coord1.x));
+        Pprime1.z = -(1.0 - abs(Pprime1.x) - abs(Pprime1.y)); // Invert original front-facing projection
+    }
+
+    vec3 p = normalize(Pprime1); // This normalization assumes Pprime was a direction vector.
+
+    float vecDist = length((p - normalize(vec3(Px, Py, Pz)))) * 206265.0;
+
+    float intensity = drawStar(vecDist, uSigma);
+
+    starBrightness *= uBrightnessScale;
+
+    // Use the brightness as the alpha value
+    return vec4(tempData, 1.0) * (starBrightness * intensity);
+} 
+
 
 mat4 rotationMatrix(float y, float x, float z) {
     // Convert angles from degrees to radians
@@ -23,10 +139,10 @@ mat4 rotationMatrix(float y, float x, float z) {
 
     // Construct the rotation matrix
     mat4 rotMatrix  = mat4(
-        cosY * cosZ, cosZ * sinX * sinY - cosX * sinZ, cosX * cosZ * sinY + sinX * sinZ,   0,
-        cosY * sinZ, cosX * cosZ + sinX * sinY * sinZ, -cosZ * sinX + cosX * sinY * sinZ,  0,
-        -sinY,       cosY * sinX,                      cosX * cosY,                        0,
-        0,           0,                                0,                                  1
+        cosY * cosZ, cosZ * sinX * sinY - cosX * sinZ, cosX * cosZ * sinY + sinX * sinZ,   0.0,
+        cosY * sinZ, cosX * cosZ + sinX * sinY * sinZ, -cosZ * sinX + cosX * sinY * sinZ,  0.0,
+        -sinY,       cosY * sinX,                      cosX * cosY,                        0.0,
+        0.0,         0.0,                              0.0,                                1.0
     );
 
     return rotMatrix;
@@ -39,15 +155,27 @@ vec4 desaturate(vec4 color, float desaturationAmount) {
 }
 
 void main() {
-    float ra = atan(vDirection.z, vDirection.x);
-    float dec = asin(vDirection.y);
+    vec3 ndir1 = (rotationMatrix(0.0, 90.0, 180.0) * vec4(vDirection, 1.0)).xyz;
+    vec3 dir = (rotationMatrix(uRotY, 90.0 - uRotX, -uRotZ) * vec4(vDirection, 1.0)).xyz;
 
-    mat4 rotMatrix = rotationMatrix(rotY, rotX, rotZ);
-    vec3 rotDirection = (rotMatrix * vec4(vDirection, 1.0)).xyz;
-    //vec4 skyColor = desaturate(texture(skyboxCubemap, rotatedDir) * speed, 0.6);
-    vec4 skyColor = texture(skyboxCubemap, rotDirection);
+    vec2 offsets[25] = vec2[](
+        vec2(0, 0), vec2(1, 0), vec2(1, 1), vec2(0, 1), vec2(-1, 1),
+        vec2(-1, 0), vec2(-1, -1), vec2(0, -1), vec2(1, -1), vec2(-2, 0),
+        vec2(2, 0), vec2(0, -2), vec2(0, 2), vec2(-2, 1), vec2(2, 1),
+        vec2(1, -2), vec2(1, 2), vec2(-2, -1), vec2(2, -1), vec2(-1, -2),
+        vec2(-1, 2), vec2(-2, -2), vec2(2, -2), vec2(-2, 2), vec2(2, 2)
+    );
+    vec4 starColor = vec4(0.0, 0.0, 0.0, 0.0);
+    for (int i = 0; i < 25; i++) {
+        starColor += getPixInfo(dir, offsets[i]);
+    }
 
-    gl_FragColor = skyColor;
+    mat4 rotMatrix = rotationMatrix(180.0, 0.0, 164.6);
+    vec3 rotDirection = (rotMatrix * vec4(dir, 1.0)).xyz;
+    vec4 skyColor = desaturate(texture(uSkyboxCubemap, rotDirection) * uMwBright, 0.6);
+    //vec4 skyColor = texture(uSkyboxCubemap, dir);
+
+    gl_FragColor = skyColor + starColor;
 
     #include <tonemapping_fragment>
 	#include <colorspace_fragment>
--- src/skybox.js
+++ src/skybox.js
@@ -1,56 +1,84 @@
 
 import * as THREE from 'three';
+import GUI from 'lil-gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import skyVertexShader from './shaders/skybox/skyVertex.glsl';
 import skyFragmentShader from './shaders/skybox/skyFragment.glsl';
 
-export class Sky extends THREE.Mesh {
-    constructor() {
-        const shader = Sky.SkyShader;
+var scene, renderer, camera, controls, skybox, effectController;
 
+export class Sky extends THREE.Mesh {
+    constructor(textures) {
+        for (let i = 0; i <= 2; i++) {
+            textures[i].format = THREE.RedFormat;
+            textures[i].type = THREE.HalfFloatType;
+            textures[i].minFilter = THREE.LinearFilter;
+            textures[i].magFilter = THREE.LinearFilter;
+            textures[i].needsUpdate = true;
+        }
+
+        const uniforms = {
+            'uYOffData': { type: 't', value: textures[0] },
+            'uXOffData': { type: 't', value: textures[1] },
+            'uMagData': { type: 't', value: textures[2] },
+            'uTempData': { type: 't', value: textures[3] },
+            'uPixelSize': { type: 'f', value: 1024.0 },
+            'uSigma': { type: 'f', value: 75.0 },
+            'uScaleFactor': { type: 'f', value: 0.0 },
+            'uBrightnessScale': { type: 'f', value: 8.0 },
+            'uSkyboxCubemap': { type: 't', value: textures[4] },
+            'uRotY': { type: 'f', value: 0.0 },
+            'uRotX': { type: 'f', value: 0.0 },
+            'uRotZ': { type: 'f', value: 0.0 },
+            'uMwBright': { type: 'f', value: 0.0 },
+        };
         const material = new THREE.ShaderMaterial({
-            name: shader.name,
-            uniforms: THREE.UniformsUtils.clone( shader.uniforms ),
+            name: 'StarShader',
+            uniforms: uniforms,
             vertexShader: skyVertexShader,
             fragmentShader: skyFragmentShader,
             side: THREE.BackSide,
             depthWrite: false
         });
-        super( new THREE.BoxGeometry(1, 1, 1), material );
-
+        super( new THREE.SphereGeometry(1, 32, 32), material );
         this.isSky = true;
+        
     }
 
 }
 
+function loadTexture(url) {
+    return new Promise((resolve, reject) => {
+        const loader = new THREE.TextureLoader();
+        loader.load(
+            url,
+            texture => {
+                // Resolve the promise with the loaded texture
+                resolve(texture);
+            },
+            undefined,
+            error => {
+                // Reject the promise if there's an error
+                reject(new Error(`Failed to load texture from ${url}: ${error.message}`));
+            }
+        );
+    });
+}
+
 function loadCubemap() {
     const prefix = 'sky_pos';
+    const extension = 'png';
     const loader = new THREE.CubeTextureLoader();
     const texture = loader.load([
-        `./skybox/${prefix}_px.png`, // positive x
-        `./skybox/${prefix}_nx.png`, // negative x
-        `./skybox/${prefix}_py.png`, // positive y
-        `./skybox/${prefix}_ny.png`, // negative y
-        `./skybox/${prefix}_pz.png`, // positive z
-        `./skybox/${prefix}_nz.png`  // negative z
+        `./skybox/${prefix}_px.${extension}`, // positive x
+        `./skybox/${prefix}_nx.${extension}`, // negative x
+        `./skybox/${prefix}_py.${extension}`, // positive y
+        `./skybox/${prefix}_ny.${extension}`, // negative y
+        `./skybox/${prefix}_pz.${extension}`, // positive z
+        `./skybox/${prefix}_nz.${extension}`  // negative z
     ]);
     return texture;
 }
-
-Sky.SkyShader = {
-    name: 'SkyShader',
-    uniforms: {
-        'skyboxCubemap': { type: 't', value: loadCubemap() },
-        'rotX': { type: 'f', value: 0.0 },
-        'rotY': { type: 'f', value: 0.0 },
-        'rotY': { type: 'f', value: 0.0 },
-        'speed': { type: 'f', value: 0.0 },
-    },
-    vertexShader: skyVertexShader,
-    fragmentShader: skyFragmentShader
-};
-
-var scene, renderer, camera, controls, skybox;
 
 async function init() {
     scene = new THREE.Scene();
@@ -69,10 +97,42 @@ async function init() {
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     scene.add(ambientLight);
 
-    skybox = new Sky();
-    skybox.scale.setScalar(450000);
-    scene.add(skybox);
-    
+    effectController = {
+        pixelSize: 1024.0,
+        sigma: 75.0,
+        scaleFactor: 0.0,
+        brightnessScale: 8.0,
+        mwBright: 0.05,
+        rotY: 0.0,
+        rotX: 36.0,
+        rotZ: 94.6
+    };
+    const gui = new GUI();
+
+    const texPromises = [
+        loadTexture('./skybox/2_yoff16BIT.png'),
+        loadTexture('./skybox/2_xoff16BIT.png'),
+        loadTexture('./skybox/2_Vmap16BIT.png'),
+        loadTexture('./skybox/2_Tmap8bit.png'),
+        Promise.resolve(loadCubemap())
+    ]
+    Promise.all(texPromises).then((textures) => {
+        skybox = new Sky(textures);
+        skybox.scale.setScalar(450000);
+        scene.add(skybox);
+
+        gui.add( effectController, 'sigma', 0.0, 500.0, 0.1 ).onChange( guiChanged );
+        gui.add( effectController, 'scaleFactor', 0.0, 10.0, 0.01 ).onChange( guiChanged );
+        gui.add( effectController, 'brightnessScale', 0.0, 1000.0, 0.1 ).onChange( guiChanged );
+        gui.add( effectController, 'mwBright', 0.0, 1.0, 0.01 ).onChange( guiChanged );
+        gui.add( effectController, 'rotX', -180, 180, 1 ).onChange( guiChanged );
+        gui.add( effectController, 'rotY', -90, 90, 1 ).onChange( guiChanged );
+        gui.add( effectController, 'rotZ', -180, 180, 1 ).onChange( guiChanged );
+        guiChanged();
+    })
+    .catch(error => {
+        console.error('An error occurred while loading one of the textures:', error);
+    });
 
     // Handle window resize
     window.addEventListener('resize', function () {
@@ -82,6 +142,19 @@ async function init() {
         camera.aspect = width / height;
         camera.updateProjectionMatrix();
     });
+
+}
+
+function guiChanged() {
+        
+    const uniforms = skybox.material.uniforms;
+    uniforms[ 'uSigma' ].value = effectController.sigma;
+    uniforms[ 'uScaleFactor' ].value = effectController.scaleFactor;
+    uniforms[ 'uBrightnessScale' ].value = effectController.brightnessScale;
+    uniforms[ 'uMwBright' ].value = effectController.mwBright;
+    uniforms[ 'uRotY' ].value = effectController.rotY;
+    uniforms[ 'uRotX' ].value = effectController.rotX;
+    uniforms[ 'uRotZ' ].value = effectController.rotZ;
 
 }
 
